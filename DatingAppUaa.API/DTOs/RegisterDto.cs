d78ebd643768b2b87e34cd4aed21209b5041090d
Arturo Barajas <basaar.24@gmail.com>
2021-10-01 07:47:15 -0500

    End of section Error handling

--- DatingAppUaa.API/DTOs/RegisterDto.cs
+++ DatingAppUaa.API/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace DatingAppUaa.API.DTOs
         public string Username { get; set; }
 
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
--- DatingAppUaa.API/Startup.cs
+++ DatingAppUaa.API/Startup.cs
@@ -1,9 +1,9 @@
 using DatingAppUaa.API.Extensions;
+using DatingAppUaa.API.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace DatingAppUaa.API
 {
@@ -29,12 +29,7 @@ namespace DatingAppUaa.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DatingAppUaa.API v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
